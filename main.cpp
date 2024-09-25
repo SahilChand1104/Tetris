2ab5b563629238dbb52dc505e4ba15e4b2ab2c95
Sahil <Sahil.Chand1104@gmail.com>
2024-09-24 18:52:59 -0700

    Created base game window

--- main.cpp
+++ main.cpp
@@ -5,6 +5,14 @@
 
 int main () {
     InitWindow(300, 600, "Tetris");
+    SetTargetFPS(60);
+    while(WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        
+        EndDrawing();
+
+    }
 
     CloseWindow();
 }
