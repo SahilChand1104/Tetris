37515b6f32d33f7ed5dd3b662732c8f57c09d0a9
Sahil <Sahil.Chand1104@gmail.com>
2024-09-24 20:27:56 -0700

    progress

--- grid.h
+++ grid.h
@@ -4,10 +4,13 @@ class Grid
 {
 public:
     Grid();
+    void Initialize();
+    void Print();
     int grid[20][10];
 
 private:
     int rows;
     int cols;
     int cellSize;
-}
+
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h>
+#include "grid.h"
 
 
 
@@ -7,6 +8,10 @@ int main () {
     Color purple = {255, 0, 255, 255};
     InitWindow(300, 600, "Tetris");
     SetTargetFPS(60);
+
+    Grid grid = Grid();
+    grid.Print();
+
     while(WindowShouldClose() == false)
     {
         BeginDrawing();
