5fc4b52f0a1d7fcf601366995f58a16d2f72ad60
Sahil <Sahil.Chand1104@gmail.com>
2024-09-24 18:57:54 -0700

    Added background color as purple

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 
 
 int main () {
+    Color purple = {255, 0, 255, 255};
     InitWindow(300, 600, "Tetris");
     SetTargetFPS(60);
     while(WindowShouldClose() == false)
     {
         BeginDrawing();
-        
+        ClearBackground(purple);
         EndDrawing();
 
     }
